20cecff2a938179513f4617735c45a7b06a58bbc
Arthur Liu <Arthur@unlimited.priv>
2017-01-24 15:22:37 -0500

    day 2- basic add

---
+++ ADBasicForm/Form_Post_MVC/Models/DirectoryModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Form_Post_MVC.Models
+{
+    public class DirectoryModel
+    {
+        /// <summary>
+        /// Gets or sets PersonId.
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets Name.
+        /// </summary>
+        public string Name { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets CN.
+        /// </summary>
+        public string CN { get; set; }
+
+        /// <summary>
+        /// Gets or sets samAccountName.
+        /// </summary>
+        public string SamAccountName { get; set; }
+
+        /// <summary>
+        /// Gets or sets Gender.
+        /// </summary>
+        public string Gender { get; set; }
+
+        /// <summary>
+        /// Gets or sets City.
+        /// </summary>
+        public string City { get; set; }
+
+
+
+        /// OU:
+        /// 
+        /// <summary>
+        /// Gets or sets OU.
+        /// </summary>
+        public string OU { get; set; }
+
+        /// <summary>
+        /// Gets or sets OUDescription.
+        /// </summary>
+        public string OUDescription { get; set; }
+
+        /// User:
+        /// 
+        /// <summary>
+        /// Gets or sets OU.
+        /// </summary>
+        public string UserName { get; set; }
+
+    
+
+
+    }
+}
